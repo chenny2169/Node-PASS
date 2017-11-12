f450f807fe0bf5d277392ce8eb320631883a67cd
leoliu <cubemail88@gmail.com>
2017-11-12 17:43:01 +0800

    完成批改作業並可以藉由批改來更新同學的分數 1.新增 views/markPersonHomework.ejs 批改特定同學的ui 2.在 routes/markHomework.js 加入檢查檢查特定同學的作業狀態、更新同學的作業分數 3.

--- routes/markHomework.js
+++ routes/markHomework.js
@@ -5,16 +5,33 @@ const GradesDB = require('../models/grades')
 /* GET grades home page. */
 router.get('/', function(req, res) {
     GradesDB.find({}).then(function(result){
-      console.log(result)
-      res.render('markHomework', { title:'Mark homework',  result :result})
-    })
-});
-
-//Monk for adding user info for grades
-router.post('/monkData', function(req, res){
-  GradesDB.create(req.body).then(function(result){
-     console.log(result)
+    console.log(result)
+    res.render('markHomework', { title:'批改作業專區',  result :result})
   })
+})
+
+//Mock for adding user info for grades
+router.post('/monkData', function(req, res){
+    GradesDB.create(req.body).then(function(result){
+    console.log(result)
+  })
+})
+
+// get specific student hw info
+router.get('/person/info/:id', function(req, res){
+    GradesDB.find({"studentID":req.params.id}).then(function(result){
+    console.log(result)
+    res.render('markPersonHomework',{title : '作業狀態', result : result})   
+  })
+})
+
+//update student hw score  
+//Query Strings 和 POST method 的表單同時使用
+router.post('/person/updateGrade', function(req, res){
+  console.log(req.body.grade)
+  console.log(req.query.studentID)
+  GradesDB.update({studentID : req.query.studentID},{$set:{homeworkGrade : req.body.grade}})
+          .then(function(result){res.redirect('/markHomework')})
 })
 
 

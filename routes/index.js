86842e30f0b8201a6861567db2a77d5d929d2390
chenny2169 <chenny2169@gmail.com>
2017-11-20 15:00:22 +0800

    1.增加login.ejs 2.完成index頁面與login頁面的連結

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+/* GET login page. */
+router.get('/login', function(req, res, next){
+  res.render('login'); 
+});
 module.exports = router;
+

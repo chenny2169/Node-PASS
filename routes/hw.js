e883c9a4bcc7607e40e1cd1dce8ad37e8fa96f16
chenny2169 <chenny2169@gmail.com>
2017-11-29 11:39:08 +0800

    1.新增"編輯作業"功能

--- routes/hw.js
+++ routes/hw.js
@@ -4,13 +4,25 @@ const HW = require('../models/homework')
 const GradeDB = require('../models/grades')
 const studentDB = require('../models/student')
 
+
+
+router.get('/edit/:_id', function(req, res){
+    HW.find({"_id":req.params._id}).then(function(result){
+        console.log(result)
+         res.render('editHomework', {title:'Edit Homework', result :result})
+    })
+})
+
+
 /* GET homework home page. */
 router.get('/:courseName', function(req, res) {
     HW.find({"courseName":req.params.courseName}).then(function(result){
      console.log(result)
-      res.render('hw', { title:'Homework' , result :result });
+      res.render('hw', { title:'Homework' , result :result })
     })
 });
+
+
 
 /* POST Add homework. */
 router.post('/addHW', function(req, res, next){
@@ -33,5 +45,18 @@ router.post('/addHW', function(req, res, next){
     res.redirect('/hw/'+req.body.courseName)
 })
 
+/* POST Edit homework. */
+router.post('/editHW', function(req, res){
+    console.log(req.body)
+    HW.update({_id : req.query.homework_uuid}, 
+        {$set:{homeworkName : req.body.homeworkName,
+             dueDate : req.body.dueDate, 
+             percentage : req.body.percentage, 
+             fileExtension : req.body.fileExtension, 
+             homeworkDescription : req.body.homeworkDescription}})
+        .then(function(result){
+            res.redirect('/hw/'+req.body.courseName)    
+    })
+})
 
 module.exports = router;

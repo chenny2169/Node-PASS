07a11f371255efb5fe47c63a5407c5a0aca0e531
leoliu <cubemail88@gmail.com>
2017-12-01 04:35:53 +0800

    1.在package.json 加入 api-test 用來跑 api 測試 2.將router/course.js裡面的邏輯移到controllers/courseController.js 3.新增course.js的api測試

--- routes/course.js
+++ routes/course.js
@@ -2,17 +2,9 @@ var express = require('express');
 var router = express.Router();
 const CourseDB = require('../models/courses')
 const HW = require('../models/homework')
+var controller = require('../controllers/courseController');
 
 /* GET homework home page. */
-router.get('/', function(req, res) {
-    CourseDB.find({}).then(function(result){
-        if(req.header('Content-Type')=='application/json'){
-            res.json({result: result})
-        }
-        else{
-            res.render('course', { title:'Course' , result :result });
-        }
-    })
-});
+router.get('/',controller.listCourseInfo)
 
 module.exports = router;

2ce0b0ad3e2ec8f77ca65ce90103b125ba5e2e58
s7130457 <s7130457@gmail.com>
2017-12-13 11:06:43 +0800

    fix bug： 1. 修正作業為空會造成系統crash的錯誤 2. 老師登出router的錯誤

--- controllers/homeworkController.js
+++ controllers/homeworkController.js
@@ -1,12 +1,12 @@
 const HW = require('../models/homework')
 
 exports.showSpecificCourseHw = function(req, res, next){
-      HW.find({"courseName" : req.params.courseName}).then(function(result){
+          HW.find({"courseName" : req.params.courseName}).then(function(result){
           if(req.header('Content-Type')=='application/json'){
             console.log(result)
             res.json({result: result})
           }else{
-            res.render('hw', { title:'Homework' , result :result })
+            res.render('hw', { title:'Homework' , result :result , courseName :req.params.courseName})
           }
     })
 }
